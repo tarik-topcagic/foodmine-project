3061c781f50f782b2048e29a7844c97bfdce1552
Tarik Topčagić <tarik.topcagic.7@gmail.com>
2023-05-30 15:23:02 +0200

    Styled food-page in food-page.component.css

--- src/app/tags/tags.component.ts
+++ src/app/tags/tags.component.ts
@@ -11,6 +11,9 @@ export class TagsComponent implements OnInit {
   @Input()
   foodPageTags?: string[];
 
+  @Input()
+  justifyContent: string = 'center';
+
   tags?: Tag[];
 
   constructor(private foodService: FoodService) { }
