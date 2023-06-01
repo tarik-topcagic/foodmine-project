0800a7f388cccfdfb300efc44e2c1c76875a9656
Tarik Topčagić <tarik.topcagic.7@gmail.com>
2023-06-01 15:38:06 +0200

    Added not-found component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { FormsModule } from '@angular/forms';
 import { TagsComponent } from './tags/tags.component';
 import { FoodPageComponent } from './food-page/food-page.component';
 import { CartPageComponent } from './cart-page/cart-page.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 // import { RatingModule } from 'ng-starrating';
 
 
@@ -22,6 +23,7 @@ import { CartPageComponent } from './cart-page/cart-page.component';
     TagsComponent,
     FoodPageComponent,
     CartPageComponent,
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
